40b5dbb7a679f21515ecfdd03ecf0f26da564a38
Nicholas Ketchum <nketchum@gmail.com>
2017-03-04 20:13:11 -0500

    comment cleanup

--- src/Controller/CustomContactController.php
+++ src/Controller/CustomContactController.php
@@ -5,31 +5,13 @@ namespace Drupal\custom_tweaks\Controller;
 use Drupal\contact\Controller\ContactController;
 use Drupal\user\UserInterface;
 use Drupal\Core\Url;
-use Drupal\Core\Render\RendererInterface;
 
 /**
- * Controller routines for contact routes.
+ * Extend controller from base contact module.
  */
 class CustomContactController extends ContactController {
-
   /**
-   * Constructs a ContactController object.
-   *
-   * @param \Drupal\Core\Render\RendererInterface $renderer
-   *   The renderer.
-   */
-  public function __construct(RendererInterface $renderer) {
-    parent::__construct($renderer);
-  }
-
-  /**
-   * Extended form constructor for the personal contact form.
-   *
-   * @param \Drupal\user\UserInterface $user
-   *   The account for which a personal contact form should be generated.
-   *
-   * @return array
-   *   The personal contact form as render array as expected by drupal_render().
+   * Extended personal page method from the base contact module.
    */
   public function contactPersonalPage(UserInterface $user) {
     // Original method returns $form.
@@ -39,20 +21,15 @@ class CustomContactController extends ContactController {
     $name_display = $this->_contactPersonalPageTitle();
     $form['#title'] = $this->t('Contact @name', array('@name' => $name_display));
 
+    // Return to drupal_render().
     return $form;
   }
 
   /**
-   * Helper function that creates a display name using the current
-   * path and a set of pre-defined user account field-ui fields.
-   *
-   * @param \Drupal\user\UserInterface $user
-   *   The account for which a personal contact form should be generated.
-   *
-   * @return string
-   *   The long-form display name of the user, or the username.
+   * Helper function that returns a concatenated display name from some
+   * optional text fields in a user account.
    */
-  protected function _contactPersonalPageTitle() {
+  private function _contactPersonalPageTitle() {
     // Use current path to grab the user object.
     $path = \Drupal::service('path.current')->getPath();
     $params = Url::fromUri("internal:" . $path)->getRouteParameters();
@@ -87,6 +64,7 @@ class CustomContactController extends ContactController {
       $name_display = $user->getUsername();
     }
 
+    // String
     return $name_display;
   }
 
